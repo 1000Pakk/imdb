d9529e487f380f7d30362884c9ec871e8eb8a209
1000Pakk <ingpanpak@gmail.com>
2025-01-18 18:44:13 +0700

    fix layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import ThemeCom from "@/components/ThemeCom";
+import Header from "../components/Header";
+import ThemeCom from "../components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressContentEditableWarning>
+      <html lang="en" suppressHydrationWarning>
         <body>
           <ThemeCom>
             <Header />
